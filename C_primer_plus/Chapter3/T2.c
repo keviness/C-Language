f1ebac798614a95345817ac44974df449b19f72c
keviness <kevin1737@163.com>
2020-02-03 23:09:21 +0800

    modify the lat commit and add chapter 11

--- C_primer_plus/Chapter3/T2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-    int number;
-    printf("Enter your number:");
-    scanf("%d", &number);
-    printf("Hello, the char of the numbner is:%c", number);
-    return 0;
-}

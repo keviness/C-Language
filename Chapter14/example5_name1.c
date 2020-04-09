abc8bdcae8d7e1a32a5e724cf36d2a9a9db59438
keviness <kevin1737@163.com>
2020-04-09 22:37:41 +0800

    update:chapter14

--- Chapter14/example5_name1.c
+++ Chapter14/example5_name1.c
@@ -1,40 +1,71 @@
 /*** names handle ***/
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define NAMELINE 80
 char * s_gets(char *st, int n);
+/*
 struct namect{
     char first[NAMELINE];
     char last[NAMELINE];
     int name_count;
 };
+*/
+struct namect{
+    char * first;
+    char * last;
+    int name_count;
+};
 
-/*
 void getinfo(struct namect *pst);            //struct pointer
 void showinfo(const struct namect *pst);
 void makeinfo(struct namect *pst);
-*/
+void cleanup(struct namect *pst);
+
+/*
 struct namect getinfo(void);                 //struct type
 void showinfo(struct namect);
 struct namect makeinfo(struct namect);
+*/
 
-/*
 int main(void)
 {
     struct namect person;
     getinfo(&person);
     makeinfo(&person);
     showinfo(&person);
-
+    cleanup(&person);
     return 0;
 }
 
+/*
 void getinfo(struct namect *pst)
 {
     puts("Enter your first name:");
     s_gets(pst->first, NAMELINE);
     puts("Enter your last name:");
     s_gets(pst->last, NAMELINE);
+}
+*/
+
+void getinfo(struct namect *pst)
+{
+    char temp[NAMELINE];
+    puts("Enter your first name:");
+    s_gets(temp, NAMELINE);
+    pst->first = (char *)malloc(strlen(temp) + 1);
+    strcpy(pst->first, temp);
+
+    puts("Enter your last name:");
+    s_gets(temp, NAMELINE);
+    pst->first = (char *)malloc(strlen(temp) + 1);
+    strcpy(pst->last, temp);
+}
+
+void cleanup(struct namect *pst)
+{
+    free(pst->first);
+    free(pst->last);
 }
 
 void makeinfo(struct namect *pst)
@@ -49,7 +80,8 @@ void showinfo(const struct namect *pst)
     printf("The last name:%s \n",pst->last);
     printf("The count of the name:%d", pst->name_count);
 }
-*/
+
+/*
 int main(void)
 {
     struct namect person;
@@ -82,6 +114,7 @@ void showinfo(struct namect info)
     printf("The last name:%s \n", info.last);
     printf("The count of the name:%d \n", info.name_count);
 }
+*/
 
 char * s_gets(char *st, int num)
 {

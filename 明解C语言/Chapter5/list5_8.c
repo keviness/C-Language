8fa90e08e80d8e39e34e82e192754cbf6f3ca163
keviness <kevin1737@163.com>
2020-12-22 22:59:10 +0800

    Update: Clarify C chapter5

---
+++ 明解C语言/Chapter5/list5_8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define MAX 10
+
+int main(void)
+{
+    int i;
+    int arr[MAX];
+    int count = 0;
+    char nums[MAX];
+
+    do
+    {
+        printf("Enter the %d number: ", count+1);
+        scanf("%d", &arr[count++]);
+        while (getchar() != '\n') continue;
+        printf("Are you continue?(Y/N):");
+        scanf("%s", nums);
+        while (getchar() != '\n') continue;
+    }while ((nums[0]!='N'||nums[0]!='n') && count<MAX);
+
+    printf("The array: \n");
+    for (i = 0; i<count; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}

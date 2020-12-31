5a50fbc427f8e1c258ad76b38d14e1612c1ce914
keviness <kevin1737@163.com>
2020-12-31 22:29:33 +0800

    Update: Chapter6 list 6-3

--- 明解C语言/Chapter6/list6_3.c
+++ 明解C语言/Chapter6/list6_3.c
@@ -3,6 +3,7 @@
 #include <time.h>
 void asctime_ex(const time_t timer);
 void ctime_ex(const time_t timer);
+int ssleep(double x);
 
 int main(void)
 {
@@ -12,6 +13,7 @@ int main(void)
     time_t current = time(NULL);
     asctime_ex(current);
     ctime_ex(current);
+    ssleep(1128.3);
     end = time(NULL);
     printf("The time used:%4.2f \n", difftime(end, start));
 
@@ -29,4 +31,18 @@ void asctime_ex(const time_t timer)
 void ctime_ex(const time_t timer)
 {
     printf("The ctime: %s\n", ctime(&timer));
+}
+
+int ssleep(double x)
+{
+    time_t t1 = time(NULL), t2;
+    do
+    {
+        if ((t2 = time(NULL)) == (time_t)-1)
+        {
+            return 0;
+        }
+    }while(difftime(t2, t1) < 0);
+
+    return 1;
 }

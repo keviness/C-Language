8b6a1789ef8e97aaeef08cc41963209921dff037
keviness <kevin1737@163.com>
2021-01-03 22:36:53 +0800

    Update: Chapter6 list6-11

---
+++ 明解C语言/Chapter6/list6_11.c
@@ -0,0 +1,13 @@
+/*** Chapter6: list6-11 ***/
+#include <stdio.h>
+
+int mday[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int dayofweek(int year, int month, int day);
+int is_leap(int year);
+int monthdays(int yrar, int month);
+void put_calendar(int y, int m);
+
+int main(void)
+{
+    
+}

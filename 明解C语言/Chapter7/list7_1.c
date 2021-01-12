006bf33db4d7b18be4d9d5d3f4ee847ccad0476d
keviness <kevin1737@163.com>
2021-01-12 23:28:52 +0800

    Update: Chapter7 list7-2

--- 明解C语言/Chapter7/list7_1.c
+++ 明解C语言/Chapter7/list7_1.c
@@ -5,30 +5,35 @@
 
 int main(void)
 {
-   int arr[SIZE] = {12, 21, 1, 2, 4, 5, 6};
-   int choice[SIZE-1] = {0};
-   srand(time(NULL));
-   int x = rand();
-   int i, j;
-   i = j = 0;
-   /*
-   for (i=j=0; i<SIZE-1; i++)
-   {
-       if (i != x)
-       {
-           choice[j++] = arr[i];
-       }
-   }
-   */
-   while (i < SIZE)
-   {
-       if (i != x)
-       {
-           choice[j++] = arr[i];
-       }
-       i++;
-   } 
+    int arr[SIZE] = {12, 21, 1, 2, 4, 5, 6};
+    int choice[SIZE-1] = {0};
+    srand(time(NULL));
+    int x = rand();
+    int i, j;
+    i = j = 0;
+    /*
+    for (i=j=0; i<SIZE-1; i++)
+    {
+        if (i != x)
+        {
+            choice[j++] = arr[i];
+        }
+    }
+    */
+    while (i < SIZE)
+    {
+        if (i != x)
+        {
+            choice[j++] = arr[i];
+        }
+        i++;
+    } 
+    printf("The array: \n");
+    for (i=0; i<j; i++)
+    {
+        printf("%d ", choice[j]);
+    }
 
-   return 0;
+    return 0;
 }
 
--- 明解C语言/Chapter7/list7_2.c
+++ 明解C语言/Chapter7/list7_2.c
@@ -1,8 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define MAXSTAGE 10
 
-int mian(void)
+int main(void)
 {
+    int i, j, stage;
+    int dgt[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int a[8];
+    double mins;
+    clock_t start, end;
+    srand(time(NULL));
+
+    printf("Enter the number that lost: \n");
+    start = clock();
+
+    for (stage=0; stage<MAXSTAGE; stage++)
+    {
+        int x = rand()%9;
+        int ans;
+        for (i=j=0; i<9; i++)
+        {
+            if (i != x)
+            {
+                a[j++] = dgt[i];
+            }
+        }
+        for (i=0; i<8; i++)
+        {
+            printf("%d ", a[i]);
+        }
+        printf(":");
+        do
+        {
+            scanf("%d", &ans);
+        } while (ans != dgt[x]);        
+    }
+    end = clock();
+
+    mins = (double)(end - start)/CLOCKS_PER_SEC;
+    printf("Using time: %2.4lf", mins);
+
+    if (mins > 25.0)
+        printf("Too delay \n");
+    else if (mins > 20.0)
+        printf("Delay \n");
+    else if (mins > 15.0)
+        printf("good \n");
+    else
+        printf("really good \n");
     
+    return 0;
 }
